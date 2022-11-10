5d77fb6b7df959b4979ab4028a1ae16e89b6d73d
NOZi <madiyar1000@mail.ru>
2022-11-10 20:13:32 +0600

    69 lesson.

--- app/Services/CartService.php
+++ app/Services/CartService.php
@@ -6,13 +6,20 @@ use Illuminate\Support\Facades\Cookie;
 use App\Cart;
 
 class CartService
-{
+{   
+    protected $cookieName = 'cart';
+
     public function getFromCookieOrCreate()
     {
-        $cartId = Cookie::get('cart');
+        $cartId = Cookie::get($this->cookieName);
 
         $cart = Cart::find($cartId);
 
         return $cart ?? Cart::create();
+    }
+
+    public function makeCookie(Cart $cart) 
+    {
+        return Cookie::make($this->cookieName, $cart->id, 7 * 24 * 60);  
     }
 }
--- resources/views/components/product-card.blade.php
+++ resources/views/components/product-card.blade.php
@@ -5,12 +5,25 @@
         <h5 class="card-title">{{$product->id}}. {{ $product->title }}</h5>
         <p class="card-text">{{ $product->description }}</p>
         <p class="card-text"><strong>{{ $product->stock}} left</strong></p>
-        <form method="POST" 
-        action="{{ route('products.carts.store', ['product' => $product->id]) }}" 
-        class="d-inline-flex">
-        @csrf
-
-        <button class="btn btn-success" type="submit">Add to Cart</button>
+        
+        @if (isset($cart))
+            <form method="POST" 
+                action="{{ route('products.carts.destroy', ['product' => $product->id, 'cart' => $cart->id]) }}" 
+                class="d-inline-flex">
+            @csrf
+            @method('DELETE')
+            <button class="btn btn-warning" type="submit">Remove From Cart</button>
+            </form>
+            
+        @else
+            
+            <form method="POST" 
+            action="{{ route('products.carts.store', ['product' => $product->id]) }}" 
+            class="d-inline-flex">
+            @csrf
+            
+            <button class="btn btn-success" type="submit">Add to Cart</button>
         </form>
+        @endif
     </div>
 </div>
