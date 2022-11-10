4b9b44d763c16ebe76bb8421fb5d41cb2e041702
NOZi <madiyar1000@mail.ru>
2022-11-10 17:55:24 +0600

    67 lesson.

--- resources/views/components/product-card.blade.php
+++ resources/views/components/product-card.blade.php
@@ -2,7 +2,7 @@
     <img class="card-img-top" src="{{ asset($product->images()->first()->path) }}" height="500">
     <div class="card-body">
         <h4 class="text-right"><strong>${{ $product->price }}</strong></h4>
-        <h5 class="card-title">{{ $product->title }}</h5>
+        <h5 class="card-title">{{$product->id}}. {{ $product->title }}</h5>
         <p class="card-text">{{ $product->description }}</p>
         <p class="card-text"><strong>{{ $product->stock}} left</strong></p>
         <form method="POST" 
