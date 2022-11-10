1544dc1d67490d92d5bcf973bf87a31c991d24a4
NOZi <madiyar1000@mail.ru>
2022-11-10 14:01:07 +0600

    63 lesson.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,6 +6,8 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use App\Product;
 use App\User;
+use App\Order;
+use App\Payment;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,8 +18,24 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        Product::factory(10)->create();
-        User::factory(10)->create();
+        $products = Product::factory(50)->create();
+
+        $users = User::factory(20)->create();
+        
+        $orders = Order::factory(10)
+                    ->make()
+                    ->each(function ($order) use ($users) {
+                        $order->customer_id = $users->random()->id;
+                        $order->save();
+
+                        $payment = Payment::factory()->make();
+                        
+                        // $payment->order_id = $order->id;
+                        // $payment->save;
+
+                        $order->payment()->save($payment); # more coolest solution to store payment with realtion to order
+                    });
+        
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
