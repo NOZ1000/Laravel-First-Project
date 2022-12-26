17e6a20d5dba94f3af1b6e256bfa6cfedfe2ea2c
NOZi <madiyar1000@mail.ru>
2022-12-27 00:08:05 +0600

    103 lesson.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\Log;
 
 class Kernel extends ConsoleKernel
 {
@@ -14,8 +15,12 @@ class Kernel extends ConsoleKernel
      * @return void
      */
     protected function schedule(Schedule $schedule)
-    {
-        // $schedule->command('inspire')->hourly();
+    {   
+        // $schedule->call(function() {
+        //     Log::debug('From Schedule');
+        // })->everyMinute();
+        
+        $schedule->command('carts:remove-old')->daily();
     }
 
     /**
